afaeef2ab3ac6e26c913229f97f5b4606b47eff6
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-01-30 20:25:03 -0800

    initial commit

---
+++ FavoriteSuperhero/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FavoriteSuperhero
+//
+//  Created by Kenny on 1/30/20.
+//  Copyright © 2020 Hazy Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
